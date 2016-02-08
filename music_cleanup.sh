087443dd3a7b89c7d04b6888a4c8801871005ae6
Stephen Kufuor <kufuo004@gmail.com>
2016-02-07 18:51:10 -0600

    Fixing bug where folders and files beginning with - or -- weren't being processed

--- music_cleanup.sh
+++ music_cleanup.sh
@@ -14,7 +14,7 @@ cd "$1"
 for i in *; do  #for each file in current directory
 	if [ -d "$i" ]
 	then
-		abs_dir=$(readlink -e "$i")
+		abs_dir=$(readlink -e -- "$i")
 #		walk_dir "$i"
 #		ls -A "$abs_dir" >> /dev/null
 #		if [ $? ];
@@ -26,7 +26,7 @@ for i in *; do  #for each file in current directory
 			echo "$abs_dir is empty"
 		fi
 	else
-		abs_path=$(readlink -f "$i")		#absolute path to file
+		abs_path=$(readlink -e -- "$i")		#absolute path to file
 #		find_dupl "$abs_path" "$base_dir"
 	fi
 cd "$1"
@@ -42,7 +42,7 @@ function find_dupl {
 	for i in *; do
 		if [ -d "$i" ]
 		then
-			abs_dir=$(readlink -e "$i")
+			abs_dir=$(readlink -e -- "$i")
 #			find_dupl "$1" "$i"
 #			ls -A "$abs_dir" >> /dev/null
 #			if [ $? ]
@@ -52,7 +52,7 @@ function find_dupl {
 				cd "$2"
 			fi
 		else
-			abs_file_path=$(readlink -f "$i")
+			abs_file_path=$(readlink -e -- "$i")
 			diff "$1" "$abs_file_path" >> /dev/null
 			if [ $? -eq 0 -a ! "$1" -ef "$i" ]
 			then
